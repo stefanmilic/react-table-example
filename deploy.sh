53ba6dff394bcb2bbb312776e06c4353ca4c4167
Stefan Milic <milicstefan6@gmail.com>
2021-10-18 18:17:28 +0200

    set aws credentials

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,20 @@ set -e
 
 if [ "$BRANCH" == "develop" ] ; then
   STAGE='dev'
+  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
+  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
 elif [ "$BRANCH" == "master" ] ; then
   STAGE='prod'
+  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PROD
+  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PROD
 fi
 
 if [ ! -z "$STAGE" ] ; then
-  echo "Deploying new version of pocket ciso on $STAGE"
+  echo "Deploying new version of football widget on $STAGE"
 
-  echo "1. deploy aws resources"
+  echo "1. set aws credentials"
+ npm run post_deploy --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY --overwrite
+  echo "2. deploy aws resources"
   npm run deploy:resources -- --stage $STAGE
 else
   echo "Stage not defined for '$BRANCH' branch, skipping..."
